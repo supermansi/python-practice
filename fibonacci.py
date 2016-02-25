c1eb87f8eee3b29f1e1d2268775cdf6cd73850a7
supermansi <mansijain2895@gmail.com>
2016-02-25 21:02:45 +0530

    tested and edited

--- fibonacci.py
+++ fibonacci.py
@@ -6,5 +6,10 @@ for i in range(0,n):
 	print(a)
 	a=b
 	b=c
-	
+#Tested till value of n=99999
+#Timed it but had to interrupt as system heated up
+#real	17m16.400s
+#user	5m13.156s
+#sys	0m9.012s
+
 
