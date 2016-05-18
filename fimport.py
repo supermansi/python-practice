823b6a95dd65d7e044dfc2dc1d8fd1aaeab75651
supermansi <mansijain2895@gmail.com>
2016-05-18 17:32:41 +0530

    added function import

---
+++ fimport.py
@@ -0,0 +1,12 @@
+#To import a module
+import math
+print math.sqrt(36)    #prints 6.0
+
+'''To import a function from a module
+from <module> import <function>
+Ex.: 
+from math import sqrt
+print sqrt(36)
+
+and from math import * eliminates the need of math.func()
+'''
