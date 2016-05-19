552f20b4d142a17fb15df1dba13ec6b699166d54
supermansi <mansijain2895@gmail.com>
2016-05-19 19:54:49 +0530

    added string and dictionaries

---
+++ string.py
@@ -0,0 +1,17 @@
+x = 45
+y = 'hello'
+print str(x) + y
+
+phone = {
+	'mansi' : 2095,
+	'mahak' : 3920,
+	'asdf' : 8523	
+}
+del phone['mahak']
+print phone
+
+lang = ['C', 'Java', 'Python']
+print "The lenght of the list is " + str(len(lang))
+print lang.index('Java')
+lang.remove('C')
+print lang
